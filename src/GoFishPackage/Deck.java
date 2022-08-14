fd581275202fefaffeef8682d3e0634f93bc304d
eric mogielnicki <mogielne@sheridancollege.ca>
2022-08-14 19:06:43 -0400

    Created branch with comments

--- src/GoFishPackage/Deck.java
+++ src/GoFishPackage/Deck.java
@@ -24,10 +24,61 @@ public class Deck {
     public void setDeck(ArrayList<String> deck) {
         this.deck = deck;
     }
+    
+    public void buildDeck(){
+
+    System.out.println("Building the deck");
+
+    for(int i = 1; i <=52; i++){
+
+        if(i <= 4){
+            deck.add("Ace");
+        }
+        else if(i <= 8){
+            deck.add("Two");
+        }
+        else if(i <= 12){
+            deck.add("Three");
+        }
+        else if(i <= 16){
+            deck.add("Four");
+        }
+        else if(i <= 20){
+            deck.add("Five");
+        }
+        else if(i <= 24){
+            deck.add("Six");
+        }
+        else if(i <= 28){
+            deck.add("Seven");
+        }
+        else if(i <= 32){
+        deck.add("Eight");
+        }
+        else if(i <= 36){
+            deck.add("Nine");
+        }
+        else if(i <= 40){
+            deck.add("Ten");
+        }
+        else if(i <= 44){
+            deck.add("Jack");
+        }
+        else if(i <= 48){
+            deck.add("Queen");
+        }
+        else if(i <= 52){
+            deck.add("King");
+        }
+        else{
+            System.out.println("The Deck has been made");
+        }
+        }
+    }
 
     public String pullCard(){
-        String card = deck[0];
+        String card = deck.get(0);
         deck.remove(0);
         return card;
-    }
+    };
 }
